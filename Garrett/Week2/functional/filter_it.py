8f1597528a1726dc377119e1267150b42b7a04a9
Garrett Skjelstad <garrettskj@gmail.com>
2015-11-17 18:21:58 -0800

    First Push

---
+++ Garrett/Week2/functional/filter_it.py
@@ -0,0 +1,14 @@
+# write a function (is_even(n)) that returns true or false depending on whether
+# a number is even or not
+
+# now write a function (filter_it) that takes a function and a list and returns
+# a new list of numbers that are even.
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+
+def filter_it(func, numlist):
+    return [ x for x in numlist if is_even(x) == True]
+
+print (filter_it(is_even, [1,2,3,4,5,6,7,8,9]))
---
+++ Garrett/Week2/functional/make_dict.py
@@ -0,0 +1,24 @@
+# write a function that takes two lists and returns a dictionary where the first
+# list is the keys and the second list is the values.
+
+# if the first list is longer than the second list then the keys should still be
+# created with the value set to the empty string
+
+# if the first list is shorter then any elements in the second list with no keys
+# should be discarded
+
+def make_dict(keys, values):
+
+
+    if len(keys) > len(values):
+
+    if len(keys) < len(values):
+
+    for key in keys:
+    
+        
+        return {key : value}
+
+if 
+
+print (make_dict('string', 2))

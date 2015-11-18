7b7ee2a1f7790938bed51a3c198af87ee3ae4be8
Garrett Skjelstad <garrettskj@gmail.com>
2015-11-17 19:54:10 -0800

    debug.py

--- Garrett/Week2/debug.py
+++ Garrett/Week2/debug.py
@@ -1,7 +1,7 @@
 # Debug the following
 
-current_time_str = input("What is the "current time" (in hours 0-23)?")
-wait_time_str = input("How many hours do you want to wait"
+current_time_str = input('What is the "current time" (in hours 0-23)?')
+wait_time_str = input("How many hours do you want to wait")
 
 current_time_int = int(current_time_str)
 wait_time_int = int(wait_time_str)
@@ -13,8 +13,8 @@ print(final_time_int)
 
 n1 = input('give a big number: ')
 n2 = input('give a little number: ')
-int(n1)
-int(n2)
+n1 = int(n1)
+n2 = int(n2)
 div = n1 // n2
 rem = n1 % n2
 print ('big divided by little is {} with remainder {}'.format(div, rem))
@@ -24,7 +24,7 @@ print ('big divided by little is {} with remainder {}'.format(div, rem))
 str_time = input("What time is it now?")
 str_wait_time = input("What is the number of nours to wait?")
 time = int(str_time)
-wai_time = int(str_wait_time)
+wait_time = int(str_wait_time)
 
 time_when_alarm_go_off = time + wait_time
 print(time_when_alarm_go_off)
@@ -32,7 +32,7 @@ print(time_when_alarm_go_off)
 ############################ NEXT #################################
 
 n = input("What time is it now (in hours)?")
-n = imt(n)
+n = int(n)
 m = input("How many hours do you want to wait?")
 m = int(m)
 q = m % 12
@@ -40,9 +40,9 @@ print("The time is now", q)
 
 ############################ NEXT #################################
 
-present_time = input("Enter the present timein hours:")
+present_time = input("Enter the present time in hours:")
 set_alarm = input("Set the hours for alarm:")
-int (present_time, set_time, alarm_time)
+present_time, set_alarm = int(present_time), int(set_alarm)
 alarm_time = present_time + set_alarm
 print(alarm_time)
 

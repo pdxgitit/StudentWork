2202e74279c6e72aa1b33f0ffd5b3ae1438214ae
Garrett Skjelstad <garrettskj@gmail.com>
2015-11-23 18:26:13 -0800

    Added Week3

---
+++ Garrett/Week3/Homework/multiply.py
@@ -0,0 +1,10 @@
+# Given two strings representing positive integers, such as "12" and "35", return a string containing their product.
+#
+# Disabled                                Accepted
+#
+# 1. use of `eval`                        1. strings
+# 2. use of ints                          2. booleans
+# 3. use of floats                        3. arrays
+#
+# Inputs will never have leading zeros, and neither should your output. Your function, multiplyMyNumbers, should
+# handle numbers with up to 5 characters, "13255" for example.

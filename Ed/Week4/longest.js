a15e9abb77e14eff9a59ccd0c1745113c60cad36
ed-g <edward.groth@gmail.com>
2015-12-01 20:28:04 -0800

    newest

--- Ed/Week4/longest.js
+++ Ed/Week4/longest.js
@@ -1,6 +1,8 @@
 function find_longest_word_in(s) {
   // s is a string
-  // find the longest word within s, words separated by spaces
+  // find the longest word within s, words
+  // consist of a string of alphabetical chracters = 'a..z' or dashes = '-'
+  // alphabetical + dashes, no numbers
   // suppose s = "The quick brown fox jumped over the lazy dog"
   // then words = ["The","quick","brown","fox","jumped"
   //              ,"over","the","lazy","dog"]
@@ -11,14 +13,20 @@ function find_longest_word_in(s) {
   // it becomes the new  longest, and its length becomes the new longest_length
 
   var words = [],
+  // /[a-z]/ means a,b,c,...,z
+  // /[-az]/ means -,a,z
+  // /[-a-z]/ means -,a,b,c,...,z
+      // nonword_regex = /[-\W]+/,
+      word_regex = /[-\w]+/,
       longest_word = "",
       current_word = "" ;
-  words = s.split(" ");
+  // words = s.split(" ");
+  words = s.split(word_regex);
 
   for (var i = 0; i < words.length; i += 1) {
       current_word = words[i];
-      // compare length of current word to longest word
-      if (current_word.length > longest_word.length) {
+      if (  (! current_word.match(/\d/))  /* toss out digit-only words */
+         && (current_word.length > longest_word.length)) {
          longest_word = current_word;
       }
   }
@@ -37,3 +45,9 @@ testfunction("The quick brown fox jumped over the lazy dog");
 
 // alphabetical, 12
 testfunction("My favorite word is alphabetical. Second favorite is numerical.");
+
+testfunction("My favorite word is 8763871644919669423. Second favorite is numerical.");
+
+testfunction("My fourth favorite word is i18n. Thrird favorite is alphanumerical.");
+
+testfunction("My sixth favorite word is alpha-betical. Fifth favorite is cat-wearing-a-hat.");

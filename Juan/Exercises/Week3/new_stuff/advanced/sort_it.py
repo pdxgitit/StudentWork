215f0d889fe8227aab23f2d6bbbf1468bc536635
oliver.bradley <oliver.bradley@forgerock.com>
2015-12-03 08:56:15 -0800

    fixes

--- Juan/Exercises/Week3/new_stuff/advanced/sort_it.py
+++
@@ -1,2 +0,0 @@
-# Write a function that takes an unordered list of integers and sorts them
-# without using any built in sort methods.  You don't need to write it recursively.
--- Juan/Exercises/Week3/new_stuff/advanced/word_count.py
+++
@@ -1,5 +0,0 @@
-Write a program that can read in a text from file and analyzes the frequency of
-each word.  It should print out the 20 most common words in order.
-
-This is most easily done with a dictionary.  I've included a text called
-alice.txt to work from.  Be sure to lower case all words and remove punctuation.

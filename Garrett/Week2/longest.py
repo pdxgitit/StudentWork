2d2a9d9834d99667a2300a0110e8ed0e02f18f34
pdxgitit <charles.webster04@gmail.com>
2015-12-02 18:33:15 -0800

    fdsdfs

--- Garrett/Week2/longest.py
+++
@@ -1,9 +0,0 @@
-# Return the length of the longest word in the provided sentence.
-# Your response should be a number.
-
-
-def find_longest_word_in(string):
-    
-
-print(find_longest_word_in("The quick brown fox jumped over the lazy dog"))
-# should return 6

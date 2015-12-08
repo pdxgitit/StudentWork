8221476351602b9cb17422248324d6e375287c29
skipamos <skipamos@gmail.com>
2015-12-07 18:30:18 -0800

    remove Skip files

--- unsorted/cleanup.py
+++
@@ -1,6 +0,0 @@
-def cleanup(x):
-    listt = []
-    for char in x:
-        listt.append(int(char))
-    return listt
-print(cleanup(['5', 4, '6']))
--- unsorted/map_it.py
+++
@@ -1,5 +0,0 @@
-# write a function (called double) that takes and integer n and returns twice
-#the value of n
-
-# Then write a function that takes a double and a list and returns a list
-# of each number doubled

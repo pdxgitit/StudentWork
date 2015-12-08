f5121669a866394d4730d1e42f71e264993b1321
skipamos <skipamos@gmail.com>
2015-12-07 18:02:49 -0800

    config MonDec7

--- Week4/freeCodeCamp.js
+++ Week4/freeCodeCamp.js
@@ -18,3 +18,38 @@
 //     return nume;
 // }
 // factorialize(5);
+
+
+//The parameter weekday is True if it is a weekday, and the parameter vacation
+// is True if we are on vacation.  We sleep in if it is not a weekday or we're
+// on vacation.  Return True if we sleep in.
+
+// function sleep_in(weekday, vacation) {
+//     if (weekday === true && vacation === true) {
+//         return true;
+//
+//     } else if (weekday === true && vacation === false) {
+//         return false;
+//     } else if (weekday === false && vacation === true) {
+//         return true;
+//     } else if (weekday === false && vacation === false) {
+//         return true;
+//     }
+// }
+//
+//
+//
+// console.log(sleep_in(false, false)); // true
+// console.log(sleep_in(true, false)); // false
+// console.log(sleep_in(false, true)); // true
+
+
+//Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+
+// function hello_name(name) {
+//     return "Hello" + name + "!";
+// }
+//
+// console.log(hello_name('Bob')) //Should return 'Hello Bob!'
+// console.log(hello_name('Alice')) //Should return 'Hello Alice!'
+// console.log(hello_name('X')) //Should return 'Hello X!'

21a90d8262fd8b2b3dbb8a441f131846df651752
skipamos <skipamos@gmail.com>
2015-12-15 13:16:03 -0800

    Mon121415

--- Skip/red_test/pricing_example.js
+++ Skip/red_test/pricing_example.js
@@ -20,30 +20,94 @@
 // };
 window.onload = function() {
     document.getElementById("add_item").onclick = addInventoryItem;
+    document.getElementById("add_stock").onclick = addStock;
+    document.getElementById("remove_stock").onclick = removeStock;
 };
+// deteremine which items are checked
+// determine the index of those items
+// update stock status of the those items
+// update the displayed list
+
+function addStock() {
+    // for ()
+
+}
+
+function removeStock() {
+
+ }
 
 var inventory = [];
 
- // constructor functioi for an inventory item
+ // constructor function for an inventory item
 
 function makeInventoryItem(name, price, in_stock) {
     this.name = name;
     this.price = price;
     this.in_stock = in_stock;
  }
-
+// adds inventory to list
  function addInventoryItem() {
      var name  = document.getElementById("name").value;
      var price = document.getElementById("price").value;
-     var in_stock = document.getElementById("in_stock").value;
+     var in_stock = document.getElementById("in_stock").checked;
+
+    //  pass those
      var new_item = new makeInventoryItem(name, price, in_stock);
 
 inventory.push(new_item);
-console.log(inventory);
 
+document.getElementById("name").value = "";
+document.getElementById("price").value = "";
+document.getElementById("in_stock").value = "";
+
+updatelist();
 
 }
 
+function updatelist() {
+    // Rerender the HTML lsit with the currrent state of the  inventroy array
+    // Empties the HTML inventory list
+    document.getElementById("inventory").innerHTML = "";
+
+    // Loops through the inventory array and adds each item to the HTML list
+
+    for (var i=0; i< inventory.length; i++) {
+        addRow(inventory[i]);
+    }
+}
+// create a function to adds a new row with each new
+// item creation on the webpage
+function addRow(new_item, index){
+    //create an extension of original table that was born in HTML to
+    // capture and reflect created new Item entries
+
+
+    var table = document.getElementById("inventory");
+    // creates new row for the table
+    var row = table.insertRow(0);
+        row.id = index;
+    // create first cell in the new row
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    // creates the 2nd cell in the new row
+    var cell3 = row.insertCell(2);
+    // creates the 3rd cell (with its own functionality)
+    var cell4 = row.insertCell(3);
+    cell1.innerHTML ="<input type='checkbox'id='" + index + "' />"
+    cell2.innerHTML = new_item.name;
+    cell3.innerHTML = "$" + new_item.price;
+    // cell3.innerHTML = new_item.in_stock;
+
+    if (new_item.in_stock === true) {
+    cell4.innerHTML = "Yes";
+    cell4.className = "true";
+}    else {
+        cell4.innerHTML = "No";
+        cell4.className = "false";
+    }
+ }
+//
 
     //  create new inventory item object
 /* list = [wood,corgi,hamster]
